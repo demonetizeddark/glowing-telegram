b20c0dc0d91ad1aea67e050f225f6477c6adbab7
morbidmeta <79297070+demonetizeddark@users.noreply.github.com>
2023-07-31 14:18:07 -0700

    Update JS and CSS

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function followlink() {
+if (navigator.userAgent.match(/Android/i)
+         || navigator.userAgent.match(/webOS/i)
+         || navigator.userAgent.match(/iPhone/i)
+         || navigator.userAgent.match(/iPad/i)
+         || navigator.userAgent.match(/iPod/i)
+         || navigator.userAgent.match(/BlackBerry/i)
+         || navigator.userAgent.match(/Windows Phone/i)) {
+            $('.container').addClass('mobi')
+         } else {
+            alert(false) ;
+         }
+
+function followlink(link) {
+    window.location = `googledocs://${link}`;
     setTimeout(function () {
-        window.location = "https://itunes.apple.com/appdir";
+        window.location = link;
     }, 25);
-    window.location = "appname://";
 }
+
+const docarray = document.querySelectorAll(".document");
+
+docarray.forEach((e) => {
+    let clickable = e.dataset.link
+    e.addEventListener('click', () => {
+        followlink(clickable)
+    })
+})
