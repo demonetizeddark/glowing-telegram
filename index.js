eaad40002a769ef304c76cba0f743c1a65220a62
MorbidMeta <georgelucero10@outlook.com>
2023-08-01 19:39:39 -0700

    update AUG 1 2023

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
-if (navigator.userAgent.match(/Android/i)
+function mobicheck() {
+    if (navigator.userAgent.match(/Android/i)
          || navigator.userAgent.match(/webOS/i)
          || navigator.userAgent.match(/iPhone/i)
          || navigator.userAgent.match(/iPad/i)
          || navigator.userAgent.match(/iPod/i)
          || navigator.userAgent.match(/BlackBerry/i)
          || navigator.userAgent.match(/Windows Phone/i)) {
-            $('.container').addClass('mobi')
-         } else {
-            alert(false) ;
-         }
+    return true
+} else {
+    return false
+}
+}
 
-function followlink(link) {
-    window.location = `googledocs://${link}`;
-    setTimeout(function () {
-        window.location = link;
-    }, 25);
+switch (mobicheck()) {
+    case true:
+        $('.container').addClass('mobi')
+        $('.overlay-container').addClass('mobi')
+        break;
+
+    default:
+        console.log("no mobile?")
+        break;
+}
+
+function goaway(link, docname) {
+    console.log('leaving site, preparing.');
+    $('.overlay').addClass('go')
+    $('.overlay-container').addClass('lapt')
+    $('.i-guess').attr('href', link)
+    $('.doc-name')
+        .text(docname)
+    setTimeout(() => {
+        window.location.href = `googledocs://${link}`
+    }, 1500)
+}
+
+
+
+function followlink(link, name) {
+    
 }
 
 const docarray = document.querySelectorAll(".document");
 
 docarray.forEach((e) => {
     let clickable = e.dataset.link
+    let docname = $(e).text()
     e.addEventListener('click', () => {
-        followlink(clickable)
+        // followlink(clickable, docname)
+        goaway(clickable, docname)
     })
 })
